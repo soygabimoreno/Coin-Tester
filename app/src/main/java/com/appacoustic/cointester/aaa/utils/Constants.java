5f361900f3975c160d485f751e06bb358e23335a
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-11-25 11:51:09 +0100

    App is completely extracted to its module

--- app/src/main/java/com/appacoustic/cointester/aaa/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.appacoustic.cointester.aaa.utils;
-
-public class Constants {
-
-    public static final String TYPEFACE_TITILLIUM_WEB_REGULAR = "TitilliumWeb-Regular.ttf";
-    public static final String TYPEFACE_TITILLIUM_WEB_SEMI_BOLD = "TitilliumWeb-SemiBold.ttf";
-}
