a15d0cf2ed2c9209d5c3b6d76d44ebbe188edaed
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-11-26 11:43:37 +0100

    Continuing extracting files and converting into Kotlin code

--- core/src/main/java/com/appacoustic/cointester/aaa/analyzer/StringBuilderNumberFormat.kt
+++ core/src/main/java/com/appacoustic/cointester/aaa/analyzer/StringBuilderNumberFormat.kt
@@ -1,140 +1,255 @@
-package com.appacoustic.cointester.aaa.analyzer;
+package com.appacoustic.cointester.aaa.analyzer
 
-import com.appacoustic.cointester.libFramework.KLog;
+import com.appacoustic.cointester.libFramework.KLog.Companion.w
 
-public class StringBuilderNumberFormat {
-
-    private static final String TAG = StringBuilderNumberFormat.class.getSimpleName();
-
-    private static final char charDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+object StringBuilderNumberFormat {
+    private val TAG = StringBuilderNumberFormat::class.java.simpleName
+    private val charDigits = charArrayOf(
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9'
+    )
 
     // Invent wheel... so we can eliminate GC
-    public static void fillInNumFixedWidthPositive(StringBuilder sb, double d, int nInt, int nFrac, char padChar) {
+    @JvmOverloads
+    fun fillInNumFixedWidthPositive(
+        sb: StringBuilder,
+        d: Double,
+        nInt: Int,
+        nFrac: Int,
+        padChar: Char = ' '
+    ) {
+        var nInt = nInt
         if (d < 0) {
-            for (int i = 0; i < nInt + nFrac + (nFrac > 0 ? 1 : 0); i++) {
-                sb.append(padChar);
+            for (i in 0 until nInt + nFrac + if (nFrac > 0) 1 else 0) {
+                sb.append(padChar)
             }
-            KLog.Companion.w("fillInNumFixedWidthPositive: negative number");
-            return;
+            w("fillInNumFixedWidthPositive: negative number")
+            return
         }
-        if (d >= Math.pow(10, nInt)) {
-            sb.append("OFL");
-            for (int i = 3; i < nInt + nFrac + (nFrac > 0 ? 1 : 0); i++) {
-                sb.append(' ');
+        if (d >= Math.pow(
+                10.0,
+                nInt.toDouble()
+            )
+        ) {
+            sb.append("OFL")
+            for (i in 3 until nInt + nFrac + if (nFrac > 0) 1 else 0) {
+                sb.append(' ')
             }
-            return;
+            return
         }
-        if (Double.isNaN(d)) {
-            sb.append("NaN");
-            for (int i = 3; i < nInt + nFrac + (nFrac > 0 ? 1 : 0); i++) {
-                sb.append(' ');
+        if (java.lang.Double.isNaN(d)) {
+            sb.append("NaN")
+            for (i in 3 until nInt + nFrac + if (nFrac > 0) 1 else 0) {
+                sb.append(' ')
             }
-            return;
+            return
         }
         while (nInt > 0) {
-            nInt--;
-            if (d < Math.pow(10, nInt) && nInt > 0) {
-                if (padChar != '\0') {
-                    sb.append(padChar);
+            nInt--
+            if (d < Math.pow(
+                    10.0,
+                    nInt.toDouble()
+                ) && nInt > 0
+            ) {
+                if (padChar != '\u0000') {
+                    sb.append(padChar)
                 }
             } else {
-                sb.append(charDigits[(int) (d / Math.pow(10, nInt) % 10.0)]);
+                sb.append(
+                    charDigits[(d / Math.pow(
+                        10.0,
+                        nInt.toDouble()
+                    ) % 10.0).toInt()]
+                )
             }
         }
         if (nFrac > 0) {
-            sb.append('.');
-            for (int i = 1; i <= nFrac; i++) {
-                sb.append(charDigits[(int) (d * Math.pow(10, i) % 10.0)]);
+            sb.append('.')
+            for (i in 1..nFrac) {
+                sb.append(
+                    charDigits[(d * Math.pow(
+                        10.0,
+                        i.toDouble()
+                    ) % 10.0).toInt()]
+                )
             }
         }
     }
 
-    public static void fillInNumFixedWidthPositive(StringBuilder sb, double d, int nInt, int nFrac) {
-        fillInNumFixedWidthPositive(sb, d, nInt, nFrac, ' ');
-    }
-
-    static void fillInNumFixedFrac(StringBuilder sb, double d, int nInt, int nFrac) {
+    @JvmStatic
+    fun fillInNumFixedFrac(
+        sb: StringBuilder,
+        d: Double,
+        nInt: Int,
+        nFrac: Int
+    ) {
+        var d = d
         if (d < 0) {
-            sb.append('-');
-            d = -d;
+            sb.append('-')
+            d = -d
         }
-        fillInNumFixedWidthPositive(sb, d, nInt, nFrac, '\0');
+        fillInNumFixedWidthPositive(
+            sb,
+            d,
+            nInt,
+            nFrac,
+            '\u0000'
+        )
     }
 
-    public static void fillInNumFixedWidth(StringBuilder sb, double d, int nInt, int nFrac) {
-        int it = sb.length();
-        sb.append(' ');
+    fun fillInNumFixedWidth(
+        sb: StringBuilder,
+        d: Double,
+        nInt: Int,
+        nFrac: Int
+    ) {
+        val it = sb.length
+        sb.append(' ')
         if (d < 0) {
-            fillInNumFixedWidthPositive(sb, -d, nInt, nFrac);
-            for (int i = it; i < sb.length(); i++) {
-                if (sb.charAt(i + 1) != ' ') {
-                    sb.setCharAt(i, '-');
-                    return;
+            fillInNumFixedWidthPositive(
+                sb,
+                -d,
+                nInt,
+                nFrac
+            )
+            for (i in it until sb.length) {
+                if (sb[i + 1] != ' ') {
+                    sb.setCharAt(
+                        i,
+                        '-'
+                    )
+                    return
                 }
             }
         }
-        fillInNumFixedWidthPositive(sb, d, nInt, nFrac);
+        fillInNumFixedWidthPositive(
+            sb,
+            d,
+            nInt,
+            nFrac
+        )
     }
 
-    static void fillInNumFixedWidthSigned(StringBuilder sb, double d, int nInt, int nFrac) {
-        int it = sb.length();
-        sb.append(' ');
-        fillInNumFixedWidthPositive(sb, Math.abs(d), nInt, nFrac);
-        for (int i = it; i < sb.length(); i++) {
-            if (sb.charAt(i + 1) != ' ') {
+    fun fillInNumFixedWidthSigned(
+        sb: StringBuilder,
+        d: Double,
+        nInt: Int,
+        nFrac: Int
+    ) {
+        val it = sb.length
+        sb.append(' ')
+        fillInNumFixedWidthPositive(
+            sb,
+            Math.abs(d),
+            nInt,
+            nFrac
+        )
+        for (i in it until sb.length) {
+            if (sb[i + 1] != ' ') {
                 if (d < 0) {
-                    sb.setCharAt(i, '-');
+                    sb.setCharAt(
+                        i,
+                        '-'
+                    )
                 } else {
-                    sb.setCharAt(i, '+');
+                    sb.setCharAt(
+                        i,
+                        '+'
+                    )
                 }
-                return;
+                return
             }
         }
     }
 
-    public static void fillInNumFixedWidthSignedFirst(StringBuilder sb, double d, int nInt, int nFrac) {
+    fun fillInNumFixedWidthSignedFirst(
+        sb: StringBuilder,
+        d: Double,
+        nInt: Int,
+        nFrac: Int
+    ) {
         if (d < 0) {
-            sb.append('-');
+            sb.append('-')
         } else {
-            sb.append('+');
+            sb.append('+')
         }
-        fillInNumFixedWidthPositive(sb, Math.abs(d), nInt, nFrac);
+        fillInNumFixedWidthPositive(
+            sb,
+            Math.abs(d),
+            nInt,
+            nFrac
+        )
     }
 
-    public static void fillInInt(StringBuilder sb, int in) {
-        if (in == 0) {
-            sb.append('0');
-            return;
+    fun fillInInt(
+        sb: StringBuilder,
+        `in`: Int
+    ) {
+        var `in` = `in`
+        if (`in` == 0) {
+            sb.append('0')
+            return
         }
-        if (in < 0) {
-            sb.append('-');
-            in = -in;
+        if (`in` < 0) {
+            sb.append('-')
+            `in` = -`in`
         }
-        int it = sb.length();
-        while (in > 0) {
-            sb.insert(it, in % 10);
-            in /= 10;
+        val it = sb.length
+        while (`in` > 0) {
+            sb.insert(
+                it,
+                `in` % 10
+            )
+            `in` /= 10
         }
     }
 
-    public static void fillTime(StringBuilder sb, double t, int nFrac) {
+    fun fillTime(
+        sb: StringBuilder,
+        t: Double,
+        nFrac: Int
+    ) {
         // in format x0:00:00.x
+        var t = t
         if (t < 0) {
-            t = -t;
-            sb.append('-');
+            t = -t
+            sb.append('-')
         }
-        double u;
+        var u: Double
         // hours
-        u = Math.floor(t / 3600.0);
-        fillInInt(sb, (int) u);
-        sb.append(':');
+        u = Math.floor(t / 3600.0)
+        fillInInt(
+            sb,
+            u.toInt()
+        )
+        sb.append(':')
         // minutes
-        t -= u * 3600;
-        u = Math.floor(t / 60.0);
-        fillInNumFixedWidthPositive(sb, u, 2, 0, '0');
-        sb.append(':');
+        t -= u * 3600
+        u = Math.floor(t / 60.0)
+        fillInNumFixedWidthPositive(
+            sb,
+            u,
+            2,
+            0,
+            '0'
+        )
+        sb.append(':')
         // seconds
-        t -= u * 60;
-        fillInNumFixedWidthPositive(sb, t, 2, nFrac, '0');
+        t -= u * 60
+        fillInNumFixedWidthPositive(
+            sb,
+            t,
+            2,
+            nFrac,
+            '0'
+        )
     }
 }
